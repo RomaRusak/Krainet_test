34e8c6786c7d33da3eb6c2075246c3d443b21ccb
RomaRusak <109284248+RomaRusak@users.noreply.github.com>
2025-01-25 16:36:38 +0300

    git init

---
+++ js/script.js
@@ -0,0 +1,32 @@
+class BurgerMenuController {
+    #burgerMenuIcon   = null;
+    #overlay          = null;
+    #navMenuCloseIcon = null;
+
+    init() {
+        this.#burgerMenuIcon   = document.querySelector('.burger-menu');
+        this.#overlay          = document.querySelector('.overlay');
+        this.#navMenuCloseIcon = document.querySelector('.nav-menu__close-icon');
+
+        this.#burgerMenuIcon.addEventListener('click', this.addActiveClass.bind(this));
+        this.#overlay.addEventListener('click', this.checkNavMenuClosest.bind(this));
+        this.#navMenuCloseIcon.addEventListener('click', this.removeActiveClass.bind(this));
+    }
+
+    checkNavMenuClosest(e) {
+        if (!e.target.closest('.nav-menu')) {
+           this.removeActiveClass();
+        }
+    }
+
+    addActiveClass() {
+        this.#overlay.classList.add('overlay-active');
+    }
+
+    removeActiveClass() {
+        this.#overlay.classList.remove('overlay-active');
+    }
+}
+
+const burgerMenuController = new BurgerMenuController;
+burgerMenuController.init();
